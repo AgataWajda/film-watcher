78cdc4c39b9fce1209e4daeb24b31517ea4a400a
Agata Wajda <agawaj98@gmail.com>
2024-07-15 00:10:14 +0200

    init first searcher view's styles

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -4,10 +4,12 @@ import { useSearchParams } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import {
   StyledForm,
+  StyledFirstFinder,
   StyledLink,
   StyledList,
   StyledInput,
   StyledButton,
+  StyledTitle,
 } from './Movies.styled';
 
 const Movies = () => {
@@ -31,17 +33,30 @@ const Movies = () => {
 
   return (
     <StyledForm onSubmit={handleChange}>
-      <StyledInput name="input" defaultValue={query}></StyledInput>
-      <StyledButton type="submit">Search</StyledButton>
-      <StyledList>
-        {moviesList.map(movie => (
-          <li key={movie.id}>
-            <StyledLink to={`/movies/${movie.id}`} state={{ from }}>
-              {movie.title}{' '}
-            </StyledLink>
-          </li>
-        ))}
-      </StyledList>
+      {!query ? (
+        <StyledFirstFinder>
+          <StyledTitle>Find your movie</StyledTitle>
+          <div>
+            <StyledInput name="input" defaultValue={query}></StyledInput>
+            <StyledButton type="submit">Search</StyledButton>
+          </div>
+        </StyledFirstFinder>
+      ) : (
+        <>
+          {' '}
+          <StyledInput name="input" defaultValue={query}></StyledInput>
+          <StyledButton type="submit">Search</StyledButton>
+          <StyledList>
+            {moviesList.map(movie => (
+              <li key={movie.id}>
+                <StyledLink to={`/movies/${movie.id}`} state={{ from }}>
+                  {movie.title}{' '}
+                </StyledLink>
+              </li>
+            ))}
+          </StyledList>
+        </>
+      )}
     </StyledForm>
   );
 };
--- src/pages/Movies/Movies.styled.js
+++ src/pages/Movies/Movies.styled.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const StyledForm = styled.form``;
+export const StyledForm = styled.form`
+  height: 100vh;
+`;
 
 export const StyledList = styled.ul`
   display: flex;
@@ -21,12 +23,37 @@ export const StyledLink = styled(Link)`
 `;
 
 export const StyledInput = styled.input`
-  margin: 20px 0px 20px 20px;
-  height: 20px;
+  margin: 20px 20px 20px 20px;
+  padding: 10px;
+  border-radius: 8px;
+  width: 300px;
   font-size: 16px;
   font-weight: 500;
+  box-shadow: 4px 8px 24px 0px rgba(66, 68, 90, 1);
+
+  &: focus {
+    border: 2px solid rgb(255, 102, 0);
+    outline: none;
+  }
 `;
 
 export const StyledButton = styled.button`
   height: 25px;
+`;
+
+export const StyledFirstFinder = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 100px;
+  align-items: center;
+`;
+
+export const StyledTitle = styled.h1`
+  margin: 0;
+  padding-bottom: 30px;
+  text-align: center;
+
+  color: #171717;
+  font-size: 50px;
+  font-weight: 900;
 `;

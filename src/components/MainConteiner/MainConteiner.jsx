4c66437381eaec6ee38e3b53ed1515912851e63a
Agata Wajda <agawaj98@gmail.com>
2024-07-19 21:12:43 +0200

    add and styled mobile menu

--- src/components/MainConteiner/MainConteiner.jsx
+++ src/components/MainConteiner/MainConteiner.jsx
@@ -4,31 +4,90 @@ import {
   StyledNav,
   StyledTitle,
   StyledWrapper,
+  StyledMenuIcon,
+  StyledButton,
+  StyledMobileMenu,
+  StyledHomeIcon,
+  StyledSearchIcon,
 } from './MainConteiner.styled';
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
-const MainConteiner = () => (
-  <div>
-    <header>
-      <StyledNav>
-        <StyledWrapper>
-          <StyledImg
-            src={require('../MainConteiner/logo512.png')}
-            alt="Film site logo"
-          />
-          <StyledTitle>Film site</StyledTitle>
-        </StyledWrapper>
-        <StyledWrapper>
-          <StyledLink to="/">Home</StyledLink>
-          <StyledLink to="/movies">Find movie</StyledLink>
-        </StyledWrapper>
-      </StyledNav>
-    </header>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Outlet />
-    </Suspense>
-  </div>
-);
+const MainConteiner = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const nodeRef = useRef(null);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 768);
+  };
+
+  const handleClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div>
+      <header>
+        <StyledNav>
+          <StyledWrapper>
+            <StyledImg
+              src={require('../MainConteiner/logo512.png')}
+              alt="Film site logo"
+            />
+            <StyledTitle>Film site</StyledTitle>
+          </StyledWrapper>
+          {!isMobile ? (
+            <StyledWrapper>
+              <StyledLink to="/">Home</StyledLink>
+              <StyledLink to="/movies">Find movie</StyledLink>
+            </StyledWrapper>
+          ) : (
+            <StyledButton
+              type="button"
+              $isOpen={isModalOpen}
+              onClick={handleClick}
+            >
+              <StyledMenuIcon></StyledMenuIcon>
+            </StyledButton>
+          )}
+        </StyledNav>
+
+        <CSSTransition
+          in={isModalOpen}
+          timeout={300}
+          classNames="menu"
+          unmountOnExit
+          nodeRef={nodeRef}
+        >
+          <StyledMobileMenu ref={nodeRef}>
+            {' '}
+            <StyledLink to="/" onClick={handleClick}>
+              <StyledHomeIcon />
+              Home
+            </StyledLink>
+            <StyledLink to="/movies" onClick={handleClick}>
+              <StyledSearchIcon />
+              Find movie
+            </StyledLink>
+          </StyledMobileMenu>
+        </CSSTransition>
+      </header>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
 
 export default MainConteiner;
--- src/components/MainConteiner/MainConteiner.styled.js
+++ src/components/MainConteiner/MainConteiner.styled.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+import { CgMenuGridO } from 'react-icons/cg';
+import { AiOutlineHome } from 'react-icons/ai';
+import { IoMdSearch } from 'react-icons/io';
 
 export const StyledImg = styled.img`
   width: 70px;
@@ -20,32 +23,45 @@ export const StyledTitle = styled.h1`
   -moz-background-clip: text;
   -moz-text-fill-color: transparent;
 
-  font-size: 36px;
+  font-size: 26px;
   font-weight: 700;
   text-decoration: none;
   margin: 0;
+
+  @media (min-width: 768px) {
+    font-size: 40px;
+    font-weight: 700;
+  }
 `;
 
 export const StyledLink = styled(NavLink)`
   color: rgb(218, 214, 214);
-  font-size: 24px;
+  font-size: 35px;
   font-weight: 700;
   text-decoration: none;
+  padding-bottom: 35px;
+  display: flex;
+  gap: 10px;
 
   &.active {
     color: orange;
   }
 
-  &:first-of-type {
-    border-right: 3px solid orange;
-    padding-right: 20px;
+  @media (min-width: 768px) {
+    font-size: 28px;
+    padding-bottom: 0px;
+
+    &:first-of-type {
+      border-right: 3px solid orange;
+      padding-right: 20px;
+    }
   }
 `;
 
 export const StyledNav = styled.nav`
   display: flex;
-  padding-left: 20px;
-  padding-right: 40px;
+  padding-left: 15px;
+  padding-right: 15px;
   align-items: center;
   padding-top: 5px;
   padding-bottom: 5px;
@@ -54,6 +70,11 @@ export const StyledNav = styled.nav`
   box-shadow: 4px 8px 24px 0px rgba(66, 68, 90, 1);
   position: relative;
   z-index: 99;
+
+  @media (min-width: 768px) {
+    padding-left: 20px;
+    padding-right: 40px;
+  }
 `;
 
 export const StyledWrapper = styled.div`
@@ -64,4 +85,57 @@ export const StyledWrapper = styled.div`
   padding-bottom: 10px;
   gap: 20px;
   background-color: #171717;
+`;
+
+export const StyledMenuIcon = styled(CgMenuGridO)`
+  width: 36px;
+  height: 36px;
+
+  @media (min-width: 768px) {
+    width: 45px;
+    height: 45px;
+  }
+`;
+
+export const StyledButton = styled.button`
+  background-color: #171717;
+  border: none;
+  color: ${props => (props.$isOpen ? 'orange' : 'rgb(218, 214, 214)')};
+`;
+
+export const StyledMobileMenu = styled.div`
+  box-sizing: border-box;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #171717;
+  display: flex;
+  flex-direction: column;
+  padding: 200px 40px;
+
+  &.menu-enter {
+    transform: translateY(-100%);
+  }
+  &.menu-enter-active {
+    transform: translateY(0);
+    transition: transform 300ms ease-in-out;
+  }
+  &.menu-exit {
+    transform: translateY(0);
+  }
+  &.menu-exit-active {
+    transform: translateY(-100%);
+    transition: transform 300ms ease-in-out;
+  }
+`;
+
+export const StyledHomeIcon = styled(AiOutlineHome)`
+  width: 35px;
+  height: 35px;
+`;
+
+export const StyledSearchIcon = styled(IoMdSearch)`
+  width: 35px;
+  height: 35px;
 `;
